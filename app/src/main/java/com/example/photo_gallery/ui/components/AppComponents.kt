20d947496a2d4cceff0407086c1579a8a7479218
Shivangi <shivangit1111@gmail.com>
2024-10-12 17:24:39 +0530

    feat: create clickable component

--- app/src/main/java/com/example/photo_gallery/ui/components/AppComponents.kt
+++ app/src/main/java/com/example/photo_gallery/ui/components/AppComponents.kt
@@ -1,11 +1,16 @@
 package com.example.photo_gallery.ui.components
 
 
-
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -13,6 +18,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.photo_gallery.ui.theme.Purple40
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.photo_gallery.R
 
 
 @Composable
@@ -30,5 +39,40 @@ fun NormalTextComponent(textValue: String) {
     )
 }
 
+@Composable
+fun PhotoComponent(
+) {
+    CaptureImage()
+}
+
+
+const val TAG = "Photoscreen"
+
+@Composable
+fun CaptureImage(
+    painter: Painter = painterResource(id = R.drawable.camera),
+    modifier: Modifier = Modifier.clickable(onClick = {
+        // Action to perform
+        Log .d(TAG, "icon Clicked")
+    })
+) {
+    Row(
+        modifier = modifier.padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = modifier.size(30.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun prev() {
+    PhotoComponent()
+
+}
 
 
--- app/src/main/java/com/example/photo_gallery/ui/screens/PhotoScreen.kt
+++ app/src/main/java/com/example/photo_gallery/ui/screens/PhotoScreen.kt
@@ -1,20 +1,40 @@
+
 package com.example.photo_gallery.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.photo_gallery.ui.components.NormalTextComponent
+import com.example.photo_gallery.ui.components.PhotoComponent
 
 
 @Composable
-fun PhotoScreen(){
+fun PhotoScreen() {
 
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        NormalTextComponent("PhotoScreen")
+        color = MaterialTheme.colorScheme.background,
+
+        ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            NormalTextComponent(textValue = "ABCD")
+            PhotoComponent()
+
+        }
     }
+}
+
+@Preview
+@Composable
+fun prev() {
+    PhotoScreen()
 }
